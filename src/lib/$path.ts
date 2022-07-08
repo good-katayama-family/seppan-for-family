c46f6b7b2dc779da265e9690b8ac259a5dd7ae68
honoka <h19981029@gmail.com>
2022-07-08 23:27:21 +0900

    ➕ chartライブラリ導入 #6

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -1,4 +1,7 @@
 export const pagesPath = {
+  "chart": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/chart' as const, hash: url?.hash })
+  },
   $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
 }
 
