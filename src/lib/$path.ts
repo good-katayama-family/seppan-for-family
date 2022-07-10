f03df2ab97c61db2fc08e155f8838d024f73de22
honoka <h19981029@gmail.com>
2022-07-10 11:19:55 +0900

    ➕ supabase導入

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -1,8 +1,20 @@
 export const pagesPath = {
-  "addform": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/addform' as const, hash: url?.hash })
+  signin: {
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/signin" as const,
+      hash: url?.hash,
+    }),
   },
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
-}
+  signup: {
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/signup" as const,
+      hash: url?.hash,
+    }),
+  },
+  $url: (url?: { hash?: string }) => ({
+    pathname: "/" as const,
+    hash: url?.hash,
+  }),
+};
 
-export type PagesPath = typeof pagesPath
+export type PagesPath = typeof pagesPath;
