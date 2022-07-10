a0f3ff48eb57668cecddb9d9415429db7c7af3ed
honoka <h19981029@gmail.com>
2022-07-10 13:24:16 +0900

    🤡 データベーステスト

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -8,6 +8,9 @@ export const pagesPath = {
   "signup": {
     $url: (url?: { hash?: string }) => ({ pathname: '/signup' as const, hash: url?.hash })
   },
+  "test": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/test' as const, hash: url?.hash })
+  },
   $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
 }
 
