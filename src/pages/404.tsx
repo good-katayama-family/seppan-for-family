30a20dba91d0a5f69b9c4489583ad14f0b604082
katayama <tattu.0310@gmail.com>
2022-07-26 00:06:00 +0900

    💄 見栄えを整える #10

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,19 @@
+
+import { Image } from '@mantine/core'
 import React from 'react'
 
 const NotFound = () => {
     return (
-        <div className='text-center font-bold text-5xl mt-20 text-red-500'>404みせられねえぜ</div>
+        <div className='flex flex-col h-full mx-auto justify-center items-center space-y-8'>
+            <div className='text-center font-bold text-5xl text-[#ff0077]'>404えっち～!!!</div>
+            <div>
+                <Image src="http://d2dcan0armyq93.cloudfront.net/photo/odai/600/5abc62003bb14f2c2ffb4bcb3b820948_600.jpg" alt='しずかちゃん'
+                    width={400}
+                    height={400}
+                    radius={999}
+                />
+            </div>
+        </div>
     )
 }
 
