42c42b7f1ae60cc24084cf1df4411e0e0b36ec75
katayama <tattu.0310@gmail.com>
2022-07-27 20:12:32 +0900

    💄 お試しカード

--- src/pages/subscription.tsx
+++ src/pages/subscription.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import type { NextPage } from "next";
 import React, { useEffect, useState } from 'react'
 import { supabase } from 'src/lib/supabase/supabase';
-import { Button, Group, Table } from '@mantine/core';
+import { Button, Card, Group, Table } from '@mantine/core';
 import { Table as Table_icon } from 'tabler-icons-react';
 import type { subsType } from "@lib/type/subs.model"
 import dayjs from "dayjs"
@@ -36,61 +36,64 @@ const Subscription: NextPage = () => {
     }, [])
 
     return (
-        <div className="w-[400px] m-auto">
-            <div className='flex justify-center'>
-                <h1 className='text-center'>サブスク管理画面</h1>
-                <Table_icon
-                    size={36}
-                    strokeWidth={2}
-                    color={'#7950f2'}
-                    className="mt-[28px]"
-                />
-            </div>
-            <Table striped
-                sx={(theme) => ({
-                    backgroundColor: theme.colors.gray[1],
-                    // '&:hover': {
-                    //     backgroundColor: theme.colors.violet[1],
-                    // },
-                })}>
-                <thead className='bg-gray-200'>
-                    <tr>
-                        <th>サービス名</th>
-                        <th>支払い期限日</th>
-                        <th>プラン</th>
-                        <th>料金</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {subsData?.map((data: subsType) => {
-                        return (
-                            <tr key={data.id}>
-                                <td>{data.subname}</td>
-                                <td>{data.deadline}</td>
-                                <td>{data.pay_period}</td>
-                                <td>{data.membership_fee.toLocaleString()}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </Table>
-            <Group position="center" mt="md">
-                <Link href="/subscriptionAdd">
-                    <a>
-                        <Button variant="light" color="violet" type="submit">
-                            追加
-                        </Button>
-                    </a>
-                </Link>
-                <Link href="/subscriptionModify">
-                    <a>
-                        <Button variant="light" color="violet" type="submit">
-                            修正
-                        </Button>
-                    </a>
-                </Link>
+        <div className="w-[500px] m-auto">
+            <Card shadow="xl" p="lg" radius="md" withBorder>
+                <div className='flex justify-center'>
+                    <h1 className='text-center'>サブスク管理画面</h1>
+                    <Table_icon
+                        size={36}
+                        strokeWidth={2}
+                        color={'#7950f2'}
+                        className="mt-[28px]"
+                    />
+                </div>
+                <Table striped
+                    sx={(theme) => ({
+                        backgroundColor: theme.colors.gray[1],
+                        // '&:hover': {
+                        //     backgroundColor: theme.colors.violet[1],
+                        // },
+                    })}>
+                    <thead className='bg-gray-200'>
+                        <tr>
+                            <th>サービス名</th>
+                            <th>支払い期限日</th>
+                            <th>プラン</th>
+                            <th>料金</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {subsData?.map((data: subsType) => {
+                            return (
+                                <tr key={data.id}>
+                                    <td>{data.subname}</td>
+                                    <td>{data.deadline}</td>
+                                    <td>{data.pay_period}</td>
+                                    <td>{data.membership_fee.toLocaleString()}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </Table>
 
-            </Group>
+                <Group position="center" mt="md">
+                    <Link href="/subscriptionAdd">
+                        <a>
+                            <Button variant="light" color="violet" type="submit">
+                                追加
+                            </Button>
+                        </a>
+                    </Link>
+                    <Link href="/subscriptionModify">
+                        <a>
+                            <Button variant="light" color="violet" type="submit">
+                                修正
+                            </Button>
+                        </a>
+                    </Link>
+
+                </Group>
+            </Card>
         </div>
     )
 }
