1cda6a8665c718292966ed4fe9005b1b6c79a2cb
honoka <h19981029@gmail.com>
2022-08-07 10:44:57 +0900

    💄 404画面

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,9 +5,9 @@ import React from 'react'
 const NotFound = () => {
     return (
         <div className='flex flex-col h-full mx-auto justify-center items-center space-y-8'>
-            <div className='text-center font-bold text-5xl text-[#ff0077]'>404えっち～!!!</div>
+            <div className='text-center font-bold text-5xl text-[#7950f2]'></div>
             <div>
-                <Image src="http://d2dcan0armyq93.cloudfront.net/photo/odai/600/5abc62003bb14f2c2ffb4bcb3b820948_600.jpg" alt='しずかちゃん'
+                <Image src="/img/404.png" alt='404エラー'
                     width={400}
                     height={400}
                     radius={999}
