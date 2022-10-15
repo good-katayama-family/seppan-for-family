b21bf2fcc8e12d965c853cdd521172f530d1cdc9
okutyann <h19981029@gmail.com>
2022-10-15 15:57:00 +0900

    fix/vercel bug

--- src/pages/subscriptionModify.tsx
+++ src/pages/subscriptionModify.tsx
@@ -7,10 +7,9 @@ import { Check, FileDatabase, TableImport } from 'tabler-icons-react';
 import { Button, Group, Modal, NumberInput, Select, Table, TextInput } from '@mantine/core';
 import dayjs from "dayjs"
 import { DatePicker } from "@mantine/dates";
-import { useForm } from "@mantine/form";
-//import { UseFormReturnType } from "@mantine/form/lib/use-form";
 import { showNotification } from "@mantine/notifications";
 import { toast } from "@lib/toast/toast";
+import { useForm } from '@mantine/form';
 import { UseFormReturnType } from "@mantine/form/lib/use-form";
 
 
